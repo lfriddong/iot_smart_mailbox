285f7ec0cb803301aa35177b37e67f65e86aac2e
Zhang-Tian-Ce <145072574+Zhang-Tian-Ce@users.noreply.github.com>
2023-11-30 01:24:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,20 @@
-import uasyncio as asyncio
+import time
 import network
-import urequests
-import json
+import socket
 import machine
 from machine import Pin, PWM
 import ssd1306
 
-# Initialize I2C and OLED
-i2c = machine.I2C(scl=Pin(5), sda=Pin(4))
-oled = ssd1306.SSD1306_I2C(128, 32, i2c)
-
-# Global variables
+# 初始化I2C对象，使用正确的引脚和扫描到的设备地址
+i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
+oled = ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x3C)  # 使用扫描到的地址
 msg_pushed = False
-protection_mode = 0  # Start with protection mode off
-servo_pin = PWM(Pin(14), freq=50)  # Servo motor connected to pin 14
 
-def update_oled(message):
-    """ Update the OLED display with the given message. """
-    oled.fill(0)  # Clear the display
-    oled.text(message, 0, 0)
-    oled.show()
+# 使用GPIO14作为伺服电机的控制引脚
+servo_pin = Pin(14)
 
-async def protection_mode_monitor():
-    global protection_mode, servo_pin
-    while True:
-        if protection_mode:
-            duty = servo_pin.duty()
-            if duty < 30:  # Adjust this threshold as needed
-                update_oled("Warning: Door open!")
-            else:
-                update_oled("Protection: ON")
-            await asyncio.sleep(1)  # Check every second
-        else:
-            update_oled("Protection: OFF")
-            await asyncio.sleep(1)  # Sleep longer when protection mode is off to save CPU
+# 初始化PWM对象
+servo = PWM(servo_pin, freq=50)  # 伺服电机通常使用50Hz的频率
 
 def do_connect():
     sta_if = network.WLAN(network.STA_IF)
@@ -43,47 +24,113 @@ def do_connect():
         sta_if.connect('JoJoHome_2.4G', 'Pigeon88')  #ssid key
         while not sta_if.isconnected():
             pass
-    update_oled('WiFi Connected')
     return sta_if.config('mac')
 
-async def send_request_to_server():
+# TCP服务器地址和端口
+TCP_SERVER_IP = '192.168.0.248'  # 服务器的IP地址
+TCP_SERVER_PORT = 8080  # 服务器的端口
+
+# 伺服电机角度转换为PWM值的函数
+def servo_duty(angle):
+    return int(650 + (angle * 10 / 9))
+
+# 打开门（将伺服电机设置为垂直状态）
+def open_door():
+    servo.duty(servo_duty(90))  # 假设90度为垂直位置
+    print('Door opened')
+
+# 关闭门（将伺服电机设置为水平状态）
+def close_door():
+    servo.duty(servo_duty(0))  # 假设0度为水平位置
+    print('Door closed')
+
+def turn_on_protection():
+    # TODO: 实现开启保护模式的逻辑
+    print('Turning on protection')
+    pass
+
+def turn_off_protection():
+    # TODO: 实现关闭保护模式的逻辑
+    print('Turning off protection')
+    pass
+
+# 修改send_tcp_message函数以处理不同的响应
+def send_tcp_message(message):
     try:
-        url = 'http://ntfy.sh/iotgroup8'
-        headers = {'Content-Type': 'application/json'}
-        data = json.dumps({"message": "REQUEST_DOOROPEN"})
-        response = urequests.post(url, data=data, headers=headers)
-        print(response.text)
-    except Exception as e:
-        print('Error sending request:', e)
-    finally:
-        await asyncio.sleep(2)  # Non-blocking sleep
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((TCP_SERVER_IP, TCP_SERVER_PORT))
+        s.send(message)
+        response = s.recv(1024)
+        s.close()
+        response = response.decode('utf-8')
+        print('Received:', response)
+        oled.fill(0)  # 清屏
+        oled.text('Received:', 0, 0)
+        oled.text(response, 0, 10)
+        oled.show()
 
-async def button_task():
-    global msg_pushed
-    pin_2 = Pin(2, Pin.IN, Pin.PULL_UP)
-    while True:
-        if not pin_2.value():  # Active low button
-            await asyncio.sleep(0.01)  # Debounce delay
-            if not pin_2.value():
-                msg_pushed = True
-                while not pin_2.value():
-                    await asyncio.sleep(0.01)  # Wait for button release
-        await asyncio.sleep(0.1)  # Polling delay
+        # 根据不同的响应执行不同的操作
+        if response == "PERMIT_DOOROPEN":
+            open_door()
+        elif response == "TURN_ON_PROTECT":
+            turn_on_protection()
+        elif response == "TURN_OFF_PROTECT":
+            turn_off_protection()
 
-async def main():
+    except OSError as e:
+        print('Error:', e)
+        oled.fill(0)
+        oled.text('Error:', 0, 0)
+        oled.text(str(e), 0, 10)
+        oled.show()
+
+def send_open_request(p):
     global msg_pushed
-    # Connect to WiFi
+    # debounce
+    active = 0
+    while active < 40:
+        if p.value() == 0:
+            active += 1
+        else:
+            return
+        time.sleep_ms(1)
+
+    msg_pushed = True
+    # 发送请求打开门的命令
+    send_tcp_message(b'REQUEST_DOOROPEN_HUZZAH')
+    time.sleep(2)
+
+def usr_permit_dooropen(p):
+    global msg_pushed
+    # debounce
+    active = 0
+    while active < 40:
+        if p.value() == 0:
+            active += 1
+        else:
+            return
+        time.sleep_ms(1)
+
+    msg_pushed = True
+    send_tcp_message(b'USR_PERMIT_DOOROPEN')
+    time.sleep(2)
+
+def main():
+    global msg_pushed
     mac_address = do_connect()
     print(mac_address)
 
-    # Start tasks
-    asyncio.create_task(protection_mode_monitor())
-    asyncio.create_task(button_task())
+    pin_2 = Pin(2, Pin.IN, Pin.PULL_UP)
+    pin_2.irq(trigger=Pin.IRQ_FALLING, handler=send_open_request)
+    pin_16 = Pin(0, Pin.IN, Pin.PULL_UP)
+    pin_16.irq(trigger=Pin.IRQ_FALLING, handler=usr_permit_dooropen)
 
     while True:
-        if msg_pushed:
-            await send_request_to_server()
-            msg_pushed = False  # Reset the flag after sending the request
-        await asyncio.sleep(0.1)  # Non-blocking sleep
 
-asyncio.run(main())
+        if msg_pushed:
+            pass
+
+
+
+if __name__ == '__main__':
+    main()
