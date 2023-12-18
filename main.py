dacc7f369bad50c0e54f736773687177beb367e8
Zhang-Tian-Ce <145072574+Zhang-Tian-Ce@users.noreply.github.com>
2023-12-18 03:03:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import time
 import ujson
 
 # 请根据实际情况配置以下参数
-WIFI_SSID = "Columbia University"
-WIFI_PASSWORD = ""
+# WIFI_SSID = "Columbia University"
+# WIFI_PASSWORD = ""
+WIFI_SSID = "JoJoHome_2.4G"
+WIFI_PASSWORD = "Pigeon88"
 SERVER_URL = "http://your_server_ip:your_server_port"
-
+ESP32_SERVER = "192.168.0.211"
 # 初始化网络连接
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
@@ -24,11 +26,13 @@ print("Connected to WiFi")
 servo_pin = 0  # 请根据实际情况配置伺服电机的引脚
 button_pin = 4  # 请根据实际情况配置按钮的引脚
 buzzer_pin_number = 5  # 替换为您连接蜂鸣器的实际引脚编号
+ir_distance_sensor_pin = 14  # 红外距离传感器的引脚配置
 
 servo = machine.PWM(machine.Pin(servo_pin), freq=50)
 button = machine.Pin(button_pin, machine.Pin.IN, machine.Pin.PULL_UP)
 buzzer_pin = machine.Pin(buzzer_pin_number, machine.Pin.OUT)
 pwm_buzzer = machine.PWM(buzzer_pin)
+ir_distance_sensor = machine.ADC(machine.Pin(ir_distance_sensor_pin))
 
 # --- 全局变量 --------------
 # 设置初始状态
@@ -92,8 +96,62 @@ def stop_buzzer():
     pwm_buzzer.deinit()  # 关闭PWM，停止发声
 
 
-def take_photo_and_upload():
+def alert():
+    """
+    在检测到非法开门时触发警报
+    """
+    start_buzzer()
+    # 可以在这里添加其他警报逻辑，如 LED 闪烁等
+    time.sleep(10)  # 警报持续时间
+    stop_buzzer()
+
+
+def read_ir_distance():
+    """
+    读取红外距离传感器GP2Y0A21YK0F的值并转换为距离（单位：厘米），还需测试。
+    """
+    # 读取ADC值
+    adc_value = ir_distance_sensor.read()
+
+    # 将ADC值转换为电压（假设ESP8266的ADC范围为0-1V）
+    voltage = adc_value * (1.0 / 1023.0)
+
+    # 转换电压为距离
+    # 这个转换公式依赖于特定传感器的特性，可能需要根据实际情况进行调整
+    distance = 27.86 * (voltage ** -1.15)
+
+    return distance
+
+
+def is_door_open():
+    """
+    判断门是否被非法打开。
+    :return: 如果门被非法打开返回 True，否则返回 False。
+    """
+    global protect_mode_status
+
+    # 仅在保护模式开启时检查门是否被打开
+    if protect_mode_status:
+        distance = read_ir_distance()
+        # 如果检测到的距离大于20厘米，则认为门被打开
+        if distance > 20:
+            return True
+
+    return False
+
+
+def take_photo():
     # 实现拍照和上传服务器的代码，例如使用 ESP 摄像头和相应的库
+    # Take picture 
+    # http://{server}/snap
+    # Take picture with LED flash 
+    # http://{server}/blitz
+
+    response = urequests.get(f"http://{ESP32_SERVER}/blitz")
+    print("Photo request sent, response:", response.text)
+
+
+def upload_photo():
     pass
 
 
@@ -130,24 +188,27 @@ def main_loop():
         if door_open_permit:
             # 执行打开门的操作
             open_door()
-            # 拍照并上传到服务器
-            take_photo_and_upload()
+            # 拍照并上传到服务器,云服务器直接访问该服务器？
+            take_photo()
+            # 上传照片
+            upload_photo()
             # 通知服务器关门确认
             send_request("door_closed_confirm")
             # 设置本地保护模式状态
             protect_mode_status = True
 
         if mannual_protect_on:
-            protect_mode_status = 1
+            protect_mode_status = True
 
         if mannual_protect_off:
-            protect_mode_status = 0
+            protect_mode_status = False
 
         # 处理保护模式
         if protect_mode_status:
-            if detect_door_open:
-                # 保护模式下，检测到门被打开时触发蜂鸣器和拍照
-                take_photo_and_upload()
+            if is_door_open():
+                alert()
+                take_photo()
+                upload_photo()
                 door_open_request = False
 
         # 延时一段时间后再次轮询
