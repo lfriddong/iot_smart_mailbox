5473946ba3a745c94daddd2d36f5c48c5de3e22c
Zhang-Tian-Ce <145072574+Zhang-Tian-Ce@users.noreply.github.com>
2023-12-29 23:55:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ import time
 import ujson
 
 # 请根据实际情况配置以下参数
-# WIFI_SSID = "Columbia University"
-# WIFI_PASSWORD = ""
-WIFI_SSID = "JoJoHome_2.4G"
-WIFI_PASSWORD = "Pigeon88"
-SERVER_URL = "http://your_server_ip:your_server_port"
-ESP32_SERVER = "192.168.0.211"
+WIFI_SSID = "Columbia University"
+WIFI_PASSWORD = ""
+SERVER_URL = "http://35.225.150.6:5000"
+
 # 初始化网络连接
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
@@ -26,13 +24,13 @@ print("Connected to WiFi")
 servo_pin = 0  # 请根据实际情况配置伺服电机的引脚
 button_pin = 4  # 请根据实际情况配置按钮的引脚
 buzzer_pin_number = 5  # 替换为您连接蜂鸣器的实际引脚编号
-ir_distance_sensor_pin = 14  # 红外距离传感器的引脚配置
+light_sensor = machine.ADC(0)  # 光敏传感器连接到 ESP8266 的 A0 引脚, A0 引脚通常对应 ADC(0)
 
 servo = machine.PWM(machine.Pin(servo_pin), freq=50)
 button = machine.Pin(button_pin, machine.Pin.IN, machine.Pin.PULL_UP)
 buzzer_pin = machine.Pin(buzzer_pin_number, machine.Pin.OUT)
 pwm_buzzer = machine.PWM(buzzer_pin)
-ir_distance_sensor = machine.ADC(machine.Pin(ir_distance_sensor_pin))
+#ir_distance_sensor = machine.ADC(machine.Pin(ir_distance_sensor_pin))
 
 # --- 全局变量 --------------
 # 设置初始状态
@@ -45,11 +43,11 @@ mannual_protect_off = False
 # huzzhah本地状态字段:
 protect_mode_status = False
 door_open_request = False
-detect_door_open = False  # 通过servo检测，代码还没实现
-
-
+detect_door_open = False # 通过servo检测，代码还没实现
 # --------------------------
 
+# ------与服务器同步通信----------------
+'''
 def send_request(action):
     global SERVER_URL
     try:
@@ -58,27 +56,42 @@ def send_request(action):
     except Exception as e:
         print("Error sending request:", e)
         return {}
+'''
+# 拉取服务端状态
+def get_server_status():
+    global door_open_permit, mannual_protect_on,mannual_protect_off
+    try:
+        response = urequests.get(SERVER_URL + "/get_status")
+        server_data = response.json()
+        door_open_permit = server_data.get("door_open_permit", False)
+        mannual_protect_on = server_data.get("mannual_protect_on", False)
+        mannual_protect_off = server_data.get("mannual_protect_off", False)
+    except Exception as e:
+        print("Error checking server status:", e)
+
+# 发送并更新服务端状态
+def set_updated_status(status_name: str, value:bool ):
+    try:
+        payload = {f'{status_name}': value}
+        response = urequests.post(SERVER_URL + "/set_status", json=payload)
+        server_response = response.json()
+        print("Server response:", server_response)
+    except Exception as e:
+        print("Error sending status to server:", e)
 
 
-def check_server_status():
-    global door_open_request, door_open_permit, protect_mode_status
-    server_data = send_request("get_status")
-    door_open_request = server_data.get("door_open_request", False)
-    door_open_permit = server_data.get("door_open_permit", False)
-    protect_mode_status = server_data.get("protect_mode_status", False)
-
-
+# -------------------------------------
 def open_door():
     global servo
     # 设置伺服电机的duty值到75，以打开门
-    servo.duty(75)
+    servo.duty(20)
     time.sleep(1)  # 等待电机运动到指定位置
 
 
 def close_door():
     global servo
     # 设置伺服电机的duty值回到20，以关闭门
-    servo.duty(20)
+    servo.duty(75)
     time.sleep(1)  # 等待电机运动回初始位置
 
 
@@ -106,59 +119,26 @@ def alert():
     stop_buzzer()
 
 
-def read_ir_distance():
-    """
-    读取红外距离传感器GP2Y0A21YK0F的值并转换为距离（单位：厘米），还需测试。
-    """
-    # 读取ADC值
-    adc_value = ir_distance_sensor.read()
+# ----------------------------------------------------
 
-    # 将ADC值转换为电压（假设ESP8266的ADC范围为0-1V）
-    voltage = adc_value * (1.0 / 1023.0)
+def take_photo_and_upload_delivery():
+    # 触发esp32 /foto路由，进而拍照并上传服务器
+    camera_url = "http://10.206.107.215"
+    try:
+        # 发送 GET 请求到 /foto 路由
+        response = urequests.get(f"{camera_url}/foto")
+        print("Server response:", response.text)
+        response.close()
+    except Exception as e:
+        print("Error triggering photo capture:", e)
 
-    # 转换电压为距离
-    # 这个转换公式依赖于特定传感器的特性，可能需要根据实际情况进行调整
-    distance = 27.86 * (voltage ** -1.15)
-
-    return distance
-
-
-def is_door_open():
-    """
-    判断门是否被非法打开。
-    :return: 如果门被非法打开返回 True，否则返回 False。
-    """
-    global protect_mode_status
-
-    # 仅在保护模式开启时检查门是否被打开
-    if protect_mode_status:
-        distance = read_ir_distance()
-        # 如果检测到的距离大于20厘米，则认为门被打开
-        if distance > 20:
-            return True
-
-    return False
-
-
-def take_photo():
-    # 实现拍照和上传服务器的代码，例如使用 ESP 摄像头和相应的库
-    # Take picture 
-    # http://{server}/snap
-    # Take picture with LED flash 
-    # http://{server}/blitz
-
-    response = urequests.get(f"http://{ESP32_SERVER}/blitz")
-    print("Photo request sent, response:", response.text)
-
-
-def upload_photo():
+def take_photo_and_upload_unauth():
     pass
 
-
+# （主动发送）处理开门请求
 def button_interrupt_handler(pin):
-    global door_open_request
+    # 去抖
     time.sleep(0.05)  # 初始延时以过滤即时噪声
-
     # 再次检查按钮状态以确认是否真的被按下
     if not pin.value():
         stable_time = time.time()
@@ -166,54 +146,72 @@ def button_interrupt_handler(pin):
             if pin.value():  # 如果按钮状态改变，重新开始计时
                 stable_time = time.time()
         # 如果按钮状态在0.2秒内保持不变，则确认为有效的按钮按下
-        door_open_request = True
+
+        # 更新服务器door_open_request字段
+        # set_updated_status('door_open_request',True) 弃用此字段
+        take_photo_and_upload_delivery()
 
 
 def main_loop():
-    global door_open_request, door_open_permit, protect_mode_status
+    global door_open_request, door_open_permit, protect_mode_status, mannual_protect_on, mannual_protect_off
     button.irq(trigger=machine.Pin.IRQ_FALLING, handler=button_interrupt_handler)
-
+    
     while True:
         # 轮询服务器状态
-        check_server_status()
+        get_server_status()
 
-        # 处理开门请求
-        if door_open_request and not door_open_permit:
-            # 向服务器发送开门请求
-            send_request("request_door_open")
-            # 在这里可以添加其他逻辑
-            door_open_request = False
-
-        # 处理开门许可
+        # （接收通知）处理开门许可
         if door_open_permit:
             # 执行打开门的操作
             open_door()
-            # 拍照并上传到服务器,云服务器直接访问该服务器？
-            take_photo()
-            # 上传照片
-            upload_photo()
-            # 通知服务器关门确认
-            send_request("door_closed_confirm")
-            # 设置本地保护模式状态
+            time.sleep(5)
+            # 响应后，恢复door_open_permit为false，并同步服务端
+            door_open_permit = False
+            set_updated_status("door_open_permit",False)
+            # 设置本地保护模式状态，并同步到服务器
             protect_mode_status = True
+            set_updated_status("protect_mode_status",True)
+            time.sleep(8)
+            close_door()
 
         if mannual_protect_on:
             protect_mode_status = True
+            mannual_protect_on = False
+            set_updated_status('mannual_protect_on',False)
 
         if mannual_protect_off:
             protect_mode_status = False
+            mannual_protect_off = False
+            set_updated_status('mannual_protect_off',False)
 
         # 处理保护模式
         if protect_mode_status:
-            if is_door_open():
+            print('protect_mode_ON')
+            light_value = light_sensor.read()
+
+            if protect_mode_status:
+                if light_value > 100:
+                    detect_door_open = True
+                    # 在这里可以添加门打开时的其他操作，例如发出警报、拍照等
+                else:
+                    detect_door_open = False
+                    # 在这里可以添加门关闭或无事发生时的操作
+
+            if detect_door_open:
+                # 保护模式下，检测到门被打开时触发蜂鸣器和拍照
+                take_photo_and_upload_delivery()
+                time.sleep(0.1)
+                take_photo_and_upload_delivery()
+                time.sleep(0.1)
+                take_photo_and_upload_delivery()
                 alert()
-                take_photo()
-                upload_photo()
-                door_open_request = False
+                set_updated_status('alarm_on',True)
+                detect_door_open = False
+        elif not protect_mode_status:
+            print('protect_mode_OFF')
 
         # 延时一段时间后再次轮询
         time.sleep(3)
-
 
 # 启动主循环
 main_loop()
